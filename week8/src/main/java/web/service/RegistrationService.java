f90ae427136c11bdc326f0699bdbee177b3a77e9
Nikhil Kalra <kalranikhil98.nk@gmail.com>
2025-05-10 18:08:47 +0530

    Added week8 folder with springws project content

---
+++ week8/src/main/java/web/service/RegistrationService.java
@@ -0,0 +1,13 @@
+package web.service;
+
+public class RegistrationService {
+
+	public static boolean register(
+			String fName, String lName, String email, String dob) {
+		System.out.println("First name: " + fName);
+		System.out.println("Last name: " + lName);
+		System.out.println("Email: " + email);
+		System.out.println("DoB (yyyy-mm-dd): " + dob);
+		return true;
+	}
+}
